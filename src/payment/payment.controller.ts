e6b09577b6ca85f46d0e5c9c75b26684fac5a426
Daler Nazarov <architect0717@gmail.com>
2025-07-17 16:11:07 +0500

    Project edited!

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -7,37 +7,43 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guard/jwt.auth.guard';
 
 @ApiTags('Payments')
 @Controller('payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() dto: CreatePaymentDto) {
     return this.paymentService.create(dto)
   }
-
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.paymentService.findAll()
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.paymentService.findOne(id)
   }
 
+  @UseGuards(AuthGuard)
   @Get('/order/:orderId')
   findByOrderId(@Param('orderId', ParseIntPipe) orderId: number) {
     return this.paymentService.findByOrderId(orderId);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -46,6 +52,7 @@ export class PaymentController {
     return this.paymentService.update(id, dto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.paymentService.remove(id);
